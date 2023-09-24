a28f8e54a3620d1488ef00f52f5159194f3d1348
Josh <6749539+hashnv@users.noreply.github.com>
2023-09-24 01:40:18 +0100

    Add modify_ext

---
+++ modify_ext/src/main.rs
@@ -0,0 +1,15 @@
+fn modify_ext(old_file:&mut String) -> usize {
+    old_file.push_str(".json");
+    return old_file.len();
+}
+
+fn main() {
+    let mut tempfile: String = String::from("myfile");
+    let mut namelen: usize = tempfile.len();
+
+    println!("filename is {} chars long", namelen);
+    
+    namelen = modify_ext(&mut tempfile);
+    println!("filename is now: {}", tempfile);
+    println!("filename is now {} chars long", namelen);
+}
