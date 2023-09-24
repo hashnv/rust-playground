09e43da18a2ccca492058118c2e59e2fddf1b9e7
Josh <6749539+hashnv@users.noreply.github.com>
2023-09-24 01:40:19 +0100

    Add dumper macro

--- fun_with_macros/src/main.rs
+++ fun_with_macros/src/main.rs
@@ -4,6 +4,17 @@ macro_rules! currency {
     };
 }
 
+macro_rules! dumper {
+    ($vect:expr) => {
+        let v: Vec<_> = $vect as Vec<_>;
+        let v_len = v.len();
+        let v_cap = v.capacity();
+        println!("len={v_len}, cap={v_cap}, cont={v:?}")
+    };
+}
+
 fn main() {
-    println!("You owe me {}", currency!(50))
+    println!("You owe me {}", currency!(50));
+    let my_vec: Vec<_> = vec!(1,2,3);
+    dumper!(my_vec);
 }
