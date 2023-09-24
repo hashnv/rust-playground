892d273e23be024620e3b14aaffd44f05d7dbc74
Josh <6749539+hashnv@users.noreply.github.com>
2023-09-24 01:40:18 +0100

    Wow...

--- county_fayre/src/main.rs
+++ county_fayre/src/main.rs
@@ -1,3 +1,58 @@
+#[derive(Default)]
+#[derive(Debug)]
+struct Prize {
+    value: usize
+}
+
+impl Prize {
+    fn new(v: usize) -> Prize {
+        Prize { value: v }
+    }
+}
+
+#[derive(Debug)]
+struct BoardSquare {
+    prize: Option<Prize>
+}
+
+impl BoardSquare {
+    fn new() -> BoardSquare {
+        BoardSquare { prize: None }
+    }
+
+    fn contains_prize(&self) -> bool {
+        self.prize.is_some()
+    }
+}
+
+#[derive(Debug)]
+struct Board {
+    grid: Vec<Vec<BoardSquare>>
+}
+
+impl Board {
+    fn new(width: usize, height: usize) -> Board {
+        let mut new_board = Board { grid: Vec::new() };
+        for _ in 0..height {
+            new_board.grid.push(Vec::new())
+        };
+        for i in new_board.grid.iter_mut(){
+            for _ in 0..width {
+                i.push(BoardSquare::new())
+            }
+        };
+        new_board
+    }
+}
+
 fn main() {
-    println!("Hello, world!")
+    let mut my_board = Board::new(1, 2);
+
+    let testing_square = &mut my_board.grid[0][0];
+    testing_square.prize = Some(Prize::new(1));
+
+    if testing_square.contains_prize() {
+        let prize = testing_square.prize.as_ref().unwrap();
+        println!("{}", prize.value)
+    }
 }
--- password_check/src/main.rs
+++ password_check/src/main.rs
@@ -1,28 +1,28 @@
-pub fn has_min_length_eight(s: &str) -> Result<(), &'static str> {
-    match s.chars().count() >= 8 {
+fn has_min_length_eight(s: &str) -> Result<(), &'static str> {
+    match s.trim().chars().count() >= 8 {
         true => Ok(()),
-        false => Err("Less than eight characters long."),
+        false => Err("is fewer than eight characters long"),
     }
 }
 
-pub fn contains_uppercase(s: &str) -> Result<(), &'static str> {
-    match s.chars().any(|x| x.is_uppercase()) {
+fn contains_uppercase(s: &str) -> Result<(), &'static str> {
+    match s.trim().chars().any(|x| x.is_uppercase()) {
         true => Ok(()),
-        false => Err("Does not contain an uppercase character."),
+        false => Err("does not contain an uppercase character"),
     }
 }
 
-pub fn contains_digit(s: &str) -> Result<(), &'static str> {
-    match s.chars().any(|x| x.is_ascii_digit()) {
+fn contains_digit(s: &str) -> Result<(), &'static str> {
+    match s.trim().chars().any(|x| x.is_ascii_digit()) {
         true => Ok(()),
-        false => Err("Does not contain a digit."),
+        false => Err("does not contain a digit"),
     }
 }
 
-pub fn contains_punctuation(s: &str) -> Result<(), &'static str> {
-    match s.chars().any(|x| !x.is_alphanumeric()) {
+fn contains_punctuation(s: &str) -> Result<(), &'static str> {
+    match s.trim().chars().any(|x| !x.is_alphanumeric()) {
         true => Ok(()),
-        false => Err("Does not contain punctuation."),
+        false => Err("does not contain punctuation"),
     }
 }
 
@@ -33,13 +33,26 @@ const PASSWORD_CHECKS: &'static [fn(&str) -> Result<(), &'static str>] = &[
     contains_punctuation,
 ];
 
-pub fn valid_password(password: &str) -> Result<(), &'static str> {
-    let results: &[Option<Result<(), &'static str>>; PASSWORD_CHECKS.len()] =
-        &[None; PASSWORD_CHECKS.len()];
+fn evaluate_password_checks(password: &str) -> Vec<Result<(), &'static str>> {
+    let mut results: [Result<(), &str>; PASSWORD_CHECKS.len()] = [Ok(()); PASSWORD_CHECKS.len()];
     for (i, check) in PASSWORD_CHECKS.iter().enumerate() {
-        results[i] = check(password);
+        let result = check(password);
+        results[i] = result;
     }
-    Ok(())
+    results.into_iter().collect()
+}
+
+pub fn valid_password(password: &str) -> Result<(), Vec<&str>> {
+    let mut results = Vec::new();
+    for ele in PASSWORD_CHECKS {
+        results.push(ele(password));
+    }
+    let errors: Vec<_> = results.into_iter().filter(Result::is_err).collect();
+    let errors: Vec<_> = errors.into_iter().map(Result::unwrap_err).collect();
+    match errors.len() == 0 {
+        true => Ok(()),
+        false => Err(errors),
+    }
 }
 
 #[cfg(test)]
@@ -47,17 +60,17 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_has_min_length_eight() {
+    fn test_has_min_length_eight_over() {
         assert!(has_min_length_eight("albatross").is_ok());
     }
 
     #[test]
-    fn test_not_has_min_length_eight() {
+    fn test_has_min_length_eight_under() {
         assert!(!has_min_length_eight("monkey").is_ok());
     }
 
     #[test]
-    fn test_is_min_length() {
+    fn test_has_min_length_eight_exact() {
         assert!(has_min_length_eight("elephant").is_ok());
     }
 
@@ -102,6 +115,8 @@ fn main() {
 
     match valid_password(&passwd) {
         Ok(_) => eprintln!("That is a valid password! :)"),
-        Err(e) => eprintln!("InvalidPasswordError: '{}'", e),
+        Err(e) => {
+            eprintln!("That password is invalid because it: \n - {}", e.join("\n - "))
+        }
     }
 }
