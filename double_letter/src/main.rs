3353f2c5a94740cb38d560518dba2290c86b8d26
Josh <6749539+hashnv@users.noreply.github.com>
2023-09-24 01:40:18 +0100

    Initial double_letter

---
+++ double_letter/src/main.rs
@@ -0,0 +1,48 @@
+use std::process;
+
+pub fn has_double(s: &str) -> bool {
+    let s = s.to_lowercase();
+    let words = s.split(' ');
+    for word in words {
+        let mut previous_char = ' ';
+        for char in word.chars() {
+            if char == previous_char {
+                return true
+            }
+            previous_char = char;
+        }
+    }
+    false
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_has_double() {
+        assert!(has_double("hello"))
+    }
+
+    #[test]
+    fn test_has_double_failure() {
+        assert!(!has_double("help"))
+    }
+}
+
+fn main() {
+    let argv: Vec<String> = std::env::args().collect();
+    let argc = argv.len();
+
+    if argc < 2 {
+        eprintln!("Not enough args supplied!");
+        process::exit(1);
+    }
+
+    let s = &argv[1];
+
+    match has_double(s) {
+        true => println!("'{}' contains a double letter.", s),
+        _ => println!("'{}' does NOT contain a double letter.", s),
+    }
+}
