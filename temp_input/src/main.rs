6db35241d77dee38669ad3e689b7c5b0d10367ee
Josh <6749539+hashnv@users.noreply.github.com>
2023-09-24 01:40:18 +0100

    Using match not expect

--- temp_input/src/main.rs
+++ temp_input/src/main.rs
@@ -4,11 +4,15 @@ fn celsius_to_fahrenheit(num: f64) -> f64 {
     (num * 1.8) + 32.0
 }
 fn main() {
-    let mut buffer = String::new();
     eprint!("Please enter a tempurature in C: ");
-    io::stdin()
-        .read_line(&mut buffer)
-        .expect("Unable to read from stdin");
+    
+    let mut buffer = String::new();
+    let line = io::stdin().read_line(&mut buffer);
+    match line {
+        Ok(_i) => (),
+        Err(..) => eprintln!("ruh roh, couldn't read stdin!"),
+    };
+
     let temp = buffer.trim().parse::<f64>();
     match temp {
         Ok(i) => println!("Temp is {} F", celsius_to_fahrenheit(i)),
