61949e2356f43ecf7fae5fd34ce069043929d2ab
Josh <6749539+hashnv@users.noreply.github.com>
2023-09-24 01:40:18 +0100

    Add temp code

--- compound_types/src/main.rs
+++ compound_types/src/main.rs
@@ -1,3 +1,23 @@
+fn mean(nums: &[f64]) -> f64 {
+    let sum: f64 = nums.iter().sum();
+    sum / nums.len() as f64
+}
+
+fn celsius_to_fahrenheit(num: f64) -> f64 {
+    (num * 1.8) + 32.0 
+}
+
 fn main() {
-    println!("Hello, world!")
+    let temps = [
+        10.2,
+        11.3,
+        10.7,
+        12.0,
+        12.0,
+        11.2,
+        11.9,
+    ];
+    let average_temp = mean(&temps);
+    println!("average_temp_celsius: {:.1} ℃", average_temp);
+    println!("average_temp_fahrenheit: {:.0} ℉", celsius_to_fahrenheit(average_temp));
 }
