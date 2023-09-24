a964b47aa3e3306d4e894b1d0119437bb85c0fba
Josh <6749539+hashnv@users.noreply.github.com>
2023-09-24 01:40:19 +0100

    Add currency macro

--- fun_with_macros/src/main.rs
+++ fun_with_macros/src/main.rs
@@ -1,3 +1,9 @@
+macro_rules! currency {
+    ($num:expr) => {
+        format!("£{:.2}", $num as f64)
+    };
+}
+
 fn main() {
-    println!("Hello, world!")
+    println!("You owe me {}", currency!(50))
 }
