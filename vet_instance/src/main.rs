e41d29cac293432115a749418a68a7a8c92b14fc
Josh <6749539+hashnv@users.noreply.github.com>
2023-09-24 01:40:18 +0100

    Add pet stuff

--- vet_instance/src/main.rs
+++ vet_instance/src/main.rs
@@ -1,3 +1,23 @@
+use std::fmt;
+
+struct Pet {
+    name: String,
+    age: u8,
+    last_bill: i32,
+}
+
+impl fmt::Debug for Pet {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}, age {}, ${}", self.name, self.age, self.last_bill)
+    }
+}
+
 fn main() {
-    println!("Hello, world!")
+    let pet1 = Pet {
+        name: String::from("Mr pet"),
+        age: 8,
+        last_bill: 8012,
+    };
+
+    println!("{:?}", pet1)
 }
