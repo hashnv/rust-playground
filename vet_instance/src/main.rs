5bb68c75243d062c5e9fb62803467eb8e43d61b8
Josh <6749539+hashnv@users.noreply.github.com>
2023-09-24 01:40:18 +0100

    Impl new

--- vet_instance/src/main.rs
+++ vet_instance/src/main.rs
@@ -6,6 +6,16 @@ struct Pet {
     last_bill: i32,
 }
 
+impl Pet {
+    fn new(name: &str, age: u8, last_bill: i32) -> Pet {
+        Pet {
+            name: String::from(name),
+            age: age,
+            last_bill: last_bill,
+        }
+    }
+}
+
 impl fmt::Debug for Pet {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "{}, age {}, ${}", self.name, self.age, self.last_bill)
@@ -13,11 +23,7 @@ impl fmt::Debug for Pet {
 }
 
 fn main() {
-    let pet1 = Pet {
-        name: String::from("Mr pet"),
-        age: 8,
-        last_bill: 8012,
-    };
+    let pet1 = Pet::new("bob", 18, 123);
 
     println!("{:?}", pet1)
 }
