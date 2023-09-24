0ec376c6a0d59f5dfbc1ee57a5eebd04fe68f110
Josh <6749539+hashnv@users.noreply.github.com>
2023-09-24 01:40:18 +0100

    Update ownership

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,4 +1,24 @@
 fn main() {
+    eg1();
+    eg2();
+}
+
+fn eg1() {
     let cat = String::from("Meow");
-    println!("{}", cat)
+    // let cat2 = cat; // breaks rule 2, type "String" has no implicit copy trait.
+    let cat2 = cat.clone(); // clone() is implemented instead.
+    println!("{}", cat);
+    println!("{}", cat2);
+}
+
+fn eg2() {
+    fn how_long(s: &String) -> usize {
+        s.len()
+    }
+
+    let cat = String::from("Meow");
+    // let length = how_long(cat); // pass by value - doesn't work ! no implicit copy
+    let length = how_long(&cat); // pass by reference
+
+    println!("{} is of length {}.", cat, length);
 }
