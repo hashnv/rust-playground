2f5b665a0f5a8b7cd9087ba143777d4a81d27254
Josh <6749539+hashnv@users.noreply.github.com>
2023-09-24 01:40:19 +0100

    Add basic hexdump

--- hexdump/src/main.rs
+++ hexdump/src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io::{self, BufReader, Read};
+
+const BUFFERSIZE: usize = 16;
+
+type HexDumpBuffer = [u8; BUFFERSIZE];
+const EMPTYBUFFER: HexDumpBuffer = [0; BUFFERSIZE];
+
+fn replace_whitespace(s: &str) -> String {
+    s.clone()
+        .chars()
+        .map(|c| match c.is_ascii_graphic() || c == ' ' {
+            true => c,
+            false => '.',
+        })
+        .collect()
+}
+
+fn main() -> io::Result<()> {
+    let file = File::open("random.bytes")?;
+    let mut reader = BufReader::new(file);
+    let mut buffer: HexDumpBuffer;
+    let mut position: usize = 0;
+    loop {
+        buffer = EMPTYBUFFER; // zero out buffer
+        let bytes_read = reader.read(&mut buffer)?;
+        if bytes_read == 0 {
+            break;
+        }
+        let utf8_decoded = String::from_utf8_lossy(&buffer);
+        let no_whitespace = replace_whitespace(&utf8_decoded);
+        println!("{:08x} {:02x?} {}", position, buffer, no_whitespace);
+        position += BUFFERSIZE;
+    }
+    Ok(())
 }
