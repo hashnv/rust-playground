8feb308ad248b48bd96d75af1d76d5977a72ebcf
Josh <6749539+hashnv@users.noreply.github.com>
2023-09-24 01:40:18 +0100

    Add simple_calc_many

---
+++ simple_calc_many/src/main.rs
@@ -0,0 +1,20 @@
+use std::process;
+
+fn main() {
+    let argv: Vec<String> = std::env::args().collect();
+    let argc = argv.len();
+
+    if argc < 3 {
+        eprintln!("Not enough args supplied!");
+        process::exit(1);
+    }
+
+    let (numbers, _errors): (Vec<_>, Vec<_>) = argv[1..]
+        .into_iter()
+        .map(|s| s.parse::<i32>())
+        .partition(Result::is_ok);
+
+    let num_sum: i32 = numbers.into_iter().map(Result::unwrap).sum();
+
+    println!("{:?}", num_sum);
+}
